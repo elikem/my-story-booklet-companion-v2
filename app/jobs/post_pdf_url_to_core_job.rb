4af92e401076b587662facee5f310142b96cfb4b
Eli Adadevoh <elikem@gmail.com>
2020-08-21 08:35:32 -0400

    Update core/companion URLs

--- app/jobs/post_pdf_url_to_core_job.rb
+++ app/jobs/post_pdf_url_to_core_job.rb
@@ -4,7 +4,7 @@ class PostPdfUrlToCoreJob < ApplicationJob
   def perform(publication_id)
     # POST w/ HTTParty using both the publication_number, pdf_url
     @publication = Publication.find(publication_id)
-    post_pdf_publication_endpoint = "#{CONFIG["core_app_domain"]}/publications/publish-pdf"
+    post_pdf_publication_endpoint = "#{CONFIG["core_app_url"]}/publications/publish-pdf"
 
     response = HTTParty.post(
       post_pdf_publication_endpoint,
@@ -18,7 +18,7 @@ class PostPdfUrlToCoreJob < ApplicationJob
 
     # Status Code 204 - No Content
     unless response.code == "204"
-      # TODO: create a pdf_posted column for when a pdf is posted and the response from the core app is OK. this allows us to 
+      # TODO: create a pdf_posted column for when a pdf is posted and the response from the core app is OK. this allows us to
       # retry posting until it is successful.
       # log response
       Rails.logger.error "ERROR: Post PDF to Core app failed. HTTP Status Code #{response.code} at #{Time.now}"
