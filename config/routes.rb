fc08bc02b311a21d4b1666f3998187b7f2c818e6
Eli Adadevoh <elikem@gmail.com>
2020-09-13 15:07:04 -0500

    Add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "pages#index"
   post "publish-idml-publication" => "publications#download_idml_publication"
   get "new-pdf-alert" => "publications#new_pdf_alert"
   get "publications/:id/pdf" => "publications#show_pdf"
